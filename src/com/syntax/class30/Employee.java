5fb7dd6c4b2bbc949f63ac6d6fda1569393c9fc4
ArzuKAYA111 <fizikuludag@gmail.com>
2020-04-21 00:11:02 -0500

    Class Update

---
+++ src/com/syntax/class30/Employee.java
@@ -0,0 +1,11 @@
+package com.syntax.class30;
+/*Create a Map that will store Employee name and salary. Write a logic to retrieve an employee who gets the highest salary. Output should be in the below format
+John Smith=$100000
+ */
+public class Employee {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
